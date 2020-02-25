06345d764138e56c825e679a7612e1e517179404
IvonGomez <61439552+IvonGomez@users.noreply.github.com>
2020-02-24 19:53:17 -0500

    Update Edit Form

--- src/views/User/Edit/EditForm.jsx
+++ src/views/User/Edit/EditForm.jsx
@@ -160,7 +160,7 @@ class EditForm extends React.Component {
                   </GridItem>
               </GridContainer>
               <GridContainer >
-                  <GridItem xs={12} sm={12} md={12}>
+                  <GridItem xs={12} sm={12} md={8}>
                     <Field
                       labelText={t("label.city")+ " *"}
                       component={CustomInputRedux}
