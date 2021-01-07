d2988a253d18e3b7f4d50ea890afbf3a66a55814
melisa1509 <61194393+melisa1509@users.noreply.github.com>
2021-01-07 17:36:38 -0500

    Fix Navigate

--- src/views/Programmbs/Show/ControlNavigation.jsx
+++ src/views/Programmbs/Show/ControlNavigation.jsx
@@ -42,26 +42,7 @@ class ControlNavigation extends React.Component {
     let { t } = this.props;
     return (
         <GridContainer justify="center">
-            <GridItem xs={3} sm={3} md={3}>            
-                    <center>
-                        <Link to={"/programmbs/show/" + previous +"/" + this.props.match.params.id}>
-                            <Button color="default" fullWidth >
-                                {t("button_previous")}
-                            </Button>  
-                        </Link> 
-                    </center>               
-            </GridItem>
-            <GridItem xs={6} sm={6} md={6}></GridItem>
-            <GridItem xs={3} sm={3} md={3}>            
-                    <center>
-                        <Link to={"/programmbs/show/" + next + "/" + this.props.match.params.id}>
-                            <Button color="rose" fullWidth >
-                                {t("button_next")}
-                            </Button>  
-                        </Link> 
-                    </center>               
-            </GridItem>
-      </GridContainer>
+        </GridContainer>
     );
   } 
 }
