884b0431117c4288986a38d8c062e80ecca70db9
melisa1509 <leonardo.mauricio.gomez@gmail.com>
2021-01-24 10:40:30 -0500

    Commit

--- src/views/Programmbs/Show/HistoryTab.jsx
+++ src/views/Programmbs/Show/HistoryTab.jsx
@@ -58,7 +58,7 @@ class HistoryTab extends React.Component {
             </SuccessBold>
             <br/>
             {
-                  programmbs.history2 !== undefined ?
+                  programmbs.history2 !== "undefined" ?
                   <a
                     href={BASE_URL +  "/web/file/"  + programmbs.history2}
                     target="_blank"
